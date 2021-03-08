d1f1c8b0afd4249d82898c839ba2bd8df9501483
karl <xiezw@ruhrtec.cn>
2021-03-08 16:59:50 +0800

    测试dubbo启动顺序

---
+++ hello-dubbo-service-user-consumer/src/main/java/com/hangz/hello/dubbo/service/user/consumer/cofig/DubboConfig.java
@@ -0,0 +1,22 @@
+// package com.hangz.hello.dubbo.service.user.consumer.cofig;
+//
+// import com.alibaba.dubbo.config.ConsumerConfig;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+//
+// /**
+//  * @author karl xie
+//  */
+// @Configuration
+// public class DubboConfig {
+//
+//
+//     @Bean
+//     public ConsumerConfig consumerConfig() {
+//         ConsumerConfig consumerConfig = new ConsumerConfig();
+//         consumerConfig.setCheck(false);
+//         consumerConfig.setTimeout(20000);
+//         return consumerConfig;
+//     }
+//
+// }
